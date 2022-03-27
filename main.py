9e7373aef968f9166b939aebcdfe7f9727de5f11
Jason Kuffler <84805346+Jkuffler@users.noreply.github.com>
2022-03-27 19:03:51 +0000

    detailed description

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ crs_per_rab = carrots/rabbits
 rabbits = 12
 print(crs_per_rab)
 
-#prints 3 because updating crs_per_rab would require coding it after the updated rabbits value
+#prints 3 because updating crs_per_rab would require coding it after the updated rabbits value very much like JS
