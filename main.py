e5a1d99f9a236e170ba0e311a0eb8a6e4ea416e0
Jason Kuffler <84805346+Jkuffler@users.noreply.github.com>
2022-03-27 19:02:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+carrots = 24
+rabbits = 8
+crs_per_rab = carrots/rabbits
+rabbits = 12
+print(crs_per_rab)
+
+#prints 3 because updating crs_per_rab would require coding it after the updated rabbits value
